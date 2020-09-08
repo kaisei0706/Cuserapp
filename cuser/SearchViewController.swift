b313acacb0404da708598a0deb796e49920c59ca
kaisei <kainana0015@gmail.com>
2020-09-08 15:27:14 +0900

    .gitignoreまで作成

---
+++ cuser/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  cuser
+//
+//  Created by 岩田海靖 on 2020/09/07.
+//  Copyright © 2020 kaisei.iwata. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ cuser/TabBarViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TabBarViewController.swift
+//  cuser
+//
+//  Created by 岩田海靖 on 2020/09/07.
+//  Copyright © 2020 kaisei.iwata. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController,UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // タブアイコンの色
+        self.tabBar.tintColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        // タブバーの背景色
+        self.tabBar.barTintColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        // UITabBarControllerDelegateプロトコルのメソッドをこのクラスで処理する。
+        self.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // タブバーのアイコンがタップされた時に呼ばれるdelegateメソッドを処理する。
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        return true
+        }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
